1e45dbf79097a150efe59934c9441ddb8b365d42
eduardodsr <eduardodsr@gmail.com>
2020-11-11 21:09:38 -0300

    05: Módulos (ngModule)

---
+++ primeiro-projeto/src/app/cursos/cursos.module.ts
@@ -0,0 +1,18 @@
+import { CursosComponent } from './cursos.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    CursosComponent
+  ],
+  exports: [
+    CursosComponent
+  ]
+})
+export class CursosModule { }
